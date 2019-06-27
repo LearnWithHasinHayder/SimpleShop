5040b9a035c86ef8eb7dd7cb1d227d68ab15e4c2
Hasin Hayder <countdraculla@gmail.com>
2019-06-28 02:48:31 +0600

    episode 11 to 13

--- functions.php
+++ functions.php
@@ -96,3 +96,37 @@ function simpleshop_subcategory_count_html( $markup ) {
 }
 
 add_filter( 'woocommerce_subcategory_count_html', 'simpleshop_subcategory_count_html' );
+
+add_filter('wp_calculate_image_sizes','__return_empty_array');
+add_filter('wp_calculate_image_srcset','__return_empty_array');
+
+remove_action('woocommerce_before_shop_loop_item_title','woocommerce_show_product_loop_sale_flash',10);
+//remove_filter('woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title',10);
+remove_filter('woocommerce_after_shop_loop_item_title','woocommerce_template_loop_rating',5);
+remove_filter('woocommerce_after_shop_loop_item_title','woocommerce_template_loop_price',10);
+remove_filter('woocommerce_after_shop_loop_item','woocommerce_template_loop_add_to_cart',10);
+
+add_action('woocommerce_before_shop_loop_item_title','woocommerce_template_loop_add_to_cart',10);
+
+function simpleshop_product_add_to_cart_text($text){
+	return '<i class="fa fa-shopping-basket"></i>';
+}
+add_filter('woocommerce_product_add_to_cart_text','simpleshop_product_add_to_cart_text');
+
+function simpleshop_before_shop_loop_item(){
+	echo '<div class="product-wrap">';
+}
+add_action('woocommerce_before_shop_loop_item','simpleshop_before_shop_loop_item');
+
+function simpleshop_before_shop_loop_item_title(){
+	echo '</div><div class="woocommerce-product-title-wrap">';
+}
+add_action('woocommerce_before_shop_loop_item_title','simpleshop_before_shop_loop_item_title');
+
+add_action('woocommerce_after_shop_loop_item_title','woocommerce_show_product_loop_sale_flash',11);
+add_action('woocommerce_after_shop_loop_item','woocommerce_template_loop_price');
+
+function simpleshop_after_shop_loop_item_title(){
+	echo '<a href="#" class="wish-list"><i class="fa fa-heart-o"></i></a></div>';
+}
+add_action('woocommerce_after_shop_loop_item_title','simpleshop_after_shop_loop_item_title');
