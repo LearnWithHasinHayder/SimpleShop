68fe227dacb6b9ab9dca1859c1a01a5b6cb87fde
Hasin Hayder <countdraculla@gmail.com>
2019-08-31 19:03:27 +0600

    CSS

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -7,13 +7,13 @@ get_header();
 ?>
 
 <?php get_template_part( 'template-parts/homepage/banner' ); ?>
-    <main class="site-main">
+    <div class="site-main">
 		<?php get_template_part( 'template-parts/homepage/product-category' ); ?>
 		<?php get_template_part( 'template-parts/homepage/products' ); ?>
 		<?php get_template_part( 'template-parts/homepage/promo' ); ?>
 		<?php get_template_part( 'template-parts/homepage/popular-product' ); ?>
 		<?php get_template_part( 'template-parts/common/offer' ); ?>
 		<?php get_template_part( 'template-parts/common/flickr' ); ?>
-    </main>
+    </div>
 
 <?php get_footer(); ?>
